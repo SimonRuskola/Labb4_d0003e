01643f1283f70f5c99db346e2085c56f01f58f55
Simon <simon.ruskola@gmail.com>
2023-02-13 09:24:01 +0100

    setup
    Co-authored-by: Fralloo <Fralloo@users.noreply.github.com>

---
+++ labb4_d0003e/main.c
@@ -0,0 +1,18 @@
+/*
+ * labb4_d0003e.c
+ *
+ * Created: 2023-02-13 09:21:43
+ * Author : simon
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
